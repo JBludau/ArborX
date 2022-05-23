c0b9c70160eb33d62e20ef2573337343a895e416
JBludau <jakob.bludau@tum.de>
2022-05-23 14:22:39 -0400

    added wrapper for BVH

--- bindings/BVH/pyArborX_BVH.cpp
+++ bindings/BVH/pyArborX_BVH.cpp
@@ -4,24 +4,28 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
 
+#include <ArborX_LinearBVH.hpp>
+
+#include "../config.hpp"
+using BVH = ArborX::BVH<MemorySpace>;
 
 namespace py = pybind11;
 
-Kokkos::View<ArborX::Point*, Kokkos::CudaSpace> vec2view(std::vector<ArborX::Point> const &in, std::string const &label = "")
-{
-  Kokkos::View<ArborX::Point*, Kokkos::CudaSpace> out(
-      Kokkos::view_alloc(label, Kokkos::WithoutInitializing), in.size());
-  Kokkos::deep_copy(out, Kokkos::View<ArborX::Point const *, Kokkos::HostSpace,
-                                      Kokkos::MemoryTraits<Kokkos::Unmanaged>>{
-                             in.data(), in.size()});
-  return out;
-}
+// Kokkos::View<ArborX::Point*, Kokkos::CudaSpace> vec2view(std::vector<ArborX::Point> const &in, std::string const &label = "")
+// {
+  // Kokkos::View<ArborX::Point*, Kokkos::CudaSpace> out(
+      // Kokkos::view_alloc(label, Kokkos::WithoutInitializing), in.size());
+  // Kokkos::deep_copy(out, Kokkos::View<ArborX::Point const *, Kokkos::HostSpace,
+                                      // Kokkos::MemoryTraits<Kokkos::Unmanaged>>{
+                             // in.data(), in.size()});
+  // return out;
+// }
 
 
 PYBIND11_MODULE(BVH, m) {
     m.attr("__name__") = "pyArborX.BVH";
 
-    m.def("ArborX::Details::sortObjects",&ArborX::Details::sortObjects<ExecutionSpace,1DintViewType,unsigned int>,
+    m.def("ArborX::Details::sortObjects",&ArborX::Details::sortObjects<ExecutionSpace,intView1DType,unsigned int>,
         py::arg("ExecutionSpace"),
         py::arg("ViewType")
         );
@@ -31,16 +35,20 @@ PYBIND11_MODULE(BVH, m) {
         .def(py::init<ExecutionSpace, Primitives,SpaceFillingCurve>(),
               py::arg("ExecutionSpace"),
               py::arg("Primitives"),
-              py::arg("SpaceFillingCurve") = ArborX::Experimental::Morton64()
+              py::arg("SpaceFillingCurve") = SpaceFillingCurve()
             )
             
-        .def("query", &BVH::query<ExecutionSpace, Predicates,1DintViewType,1DintViewType>,
+        .def("query", &BVH::query<ExecutionSpace const &,
+                                  Predicates const &,
+                                  intView1DType&,
+                                  intView1DType&>,
               py::arg("ExecutionSpace"),
               py::arg("Predicates"),
               py::arg("Indices"),
               py::arg("Offsets")
             );
         
-    m.def("vec2view", &vec2view);
+    // m.def("vec2view", &vec2view);
+}
 
 #endif
